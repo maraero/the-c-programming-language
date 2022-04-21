11589891b09521f652f4fbcc97120795b9a2b30d
maraero <themaraero@gmail.com>
2022-04-21 22:04:43 +0300

    add tas 01-14

---
+++ chapter-1-introduction/01-14.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define LETTERS 26
+#define NUMBERS 10
+
+int main()
+{
+  int i;
+  char freq[LETTERS + NUMBERS];
+
+  for (i = 0; i < LETTERS + NUMBERS; ++i)
+    freq[i] = 0;
+
+  char c;
+
+  while ((c = getchar()) != EOF)
+  {
+    if (c >= 'a' && c <= 'z')
+      ++freq[c - 'a'];
+    if (c >= '0' && c <= '9')
+      ++freq[c - '0' + LETTERS];
+  }
+
+  int j;
+
+  printf("\n");
+
+  for (i = 0; i < LETTERS + NUMBERS; ++i)
+  {
+    if (freq[i] > 0)
+    {
+      char symbol;
+      if (i < LETTERS)
+      {
+        symbol = 'a' + i;
+      }
+      else
+      {
+        symbol = '0' + i - LETTERS;
+      }
+
+      printf("%c: ", symbol);
+      for (j = 0; j < freq[i]; ++j)
+        printf("#");
+      printf("\n");
+    }
+  }
+}
